e398562dc6ea60e070b169b762cb8be7a350249b
andycow0 <andycow0@gmail.com>
2016-07-11 22:48:39 +0800

    Create a Unit Test proj. and it has a red light.

---
+++ TDD_HW2/TDD_HW2.Test/PotterShoppingCartTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_HW2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_HW2.Tests
+{
+    [TestClass()]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod()]
+        public void GetPriceTest()
+        {
+            // arrange
+            // act
+            // assert
+            Assert.Fail();
+        }
+    }
+}
---
+++ TDD_HW2/TDD_HW2/PotterShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_HW2
+{
+    public class PotterShoppingCart
+    {
+
+        public int GetPrice(int num)
+        {
+            return num * 100;
+        }
+    }
+}
